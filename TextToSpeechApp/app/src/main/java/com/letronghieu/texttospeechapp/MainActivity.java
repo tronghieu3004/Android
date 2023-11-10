b8bad046867b77dcdee45283a83254bd9f2f3b73
Le Trong Hieu <letronghieu3004.utm@gmail.com>
2023-11-10 10:25:37 +0700

    init

---
+++ TextToSpeechApp/app/src/main/java/com/letronghieu/texttospeechapp/MainActivity.java
@@ -0,0 +1,232 @@
+package com.letronghieu.texttospeechapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.speech.tts.TextToSpeech;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final int FILE_PICKER_REQUEST_CODE = 1;
+    private TextToSpeech mTTS;
+    private EditText mEditText;
+    private SeekBar mSeekBarPitch;
+    private SeekBar mSeekBarSpeed;
+    private Button mButtonSpeak,btnAddDoc;
+    private Uri imageUri;
+    private TextRecognizer textRecognizer;
+    private ImageView mAddText,mBtnDCScanner,mBtnCopy,mBtnDeleteAll;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // add id
+        mButtonSpeak = findViewById(R.id.button_speak);
+        mEditText = findViewById(R.id.edit_text);
+        mSeekBarPitch = findViewById(R.id.seek_bar_pitch);
+        mSeekBarSpeed = findViewById(R.id.seek_bar_speed);
+        mBtnCopy = findViewById(R.id.btn_copy);
+        mBtnDCScanner = findViewById(R.id.btn_dc_scanner);
+        mBtnDeleteAll = findViewById(R.id.btn_delete_all);
+        btnAddDoc = findViewById(R.id.addDoc);
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        //btn copy
+        mBtnCopy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = mEditText.getText().toString();
+                if (text.isEmpty()){
+                    showToast("There is no TEXT to copy");
+                }else {
+                    ClipboardManager clipboardManager = (ClipboardManager) getSystemService(MainActivity.this.CLIPBOARD_SERVICE);
+                    ClipData clipData = ClipData.newPlainText("Data",mEditText.getText().toString());
+                    clipboardManager.setPrimaryClip(clipData);
+                    showToast("Text copy to clipboard");
+                }
+            }
+        });
+        //btn delete all
+        mBtnDeleteAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = mEditText.getText().toString();
+                if (text.isEmpty()){
+                    showToast("No data to clear");
+                }else {
+                    mEditText.setText("");
+                }
+            }
+        });
+        //btn scanner
+        mBtnDCScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImagePicker.with(MainActivity.this)
+                        .crop()
+                        .compress(5120)
+                        .maxResultSize(3840, 2160)	
+                        .start();
+            }
+        });
+        
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.getDefault());
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        mButtonSpeak.setEnabled(true);
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+
+        mButtonSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+        
+        btnAddDoc.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("text/*"); // Chỉ chấp nhận các file văn bản
+            startActivityForResult(intent, FILE_PICKER_REQUEST_CODE);
+        });
+    }
+
+
+
+
+    private void speak() {
+        String text = mEditText.getText().toString();
+        float pitch = (float) mSeekBarPitch.getProgress() / 50;
+        if (pitch < 0.1) pitch = 0.1f;
+        float speed = (float) mSeekBarSpeed.getProgress() / 50;
+        if (speed < 0.1) speed = 0.1f;
+
+        mTTS.setPitch(pitch);
+        mTTS.setSpeechRate(speed);
+        if (text.isEmpty()) {
+            showToast("No Text to read aloud");
+        }
+        else {
+
+            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+
+        super.onDestroy();
+    }
+
+    //Recognize Text
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ImagePicker.REQUEST_CODE || resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                imageUri = data.getData();
+                showToast("Image selected");
+                recognizeText();
+            }
+        }
+        if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri fileUri = data.getData();
+                // Xử lý URI của tệp văn bản được chọn
+                readTextFromFile(fileUri);
+                showToast("Document selected");
+            }
+        }
+    }
+
+    private void readTextFromFile(Uri fileUri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(fileUri);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            inputStream.close();
+
+            // Hiển thị văn bản trong EditText
+            mEditText.setText(stringBuilder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void recognizeText(){
+        if (imageUri != null) {
+            try {
+                InputImage inputImage = InputImage.fromFilePath(MainActivity.this,imageUri);
+                Task<Text> result = textRecognizer.process(inputImage)
+                        .addOnSuccessListener(new OnSuccessListener<Text>() {
+                            @Override
+                            public void onSuccess(Text text) {
+                                String recognizeText = text.getText();
+                                mEditText.setText(recognizeText);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                showToast(e.getMessage());
+                            }
+                        });
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    private void showToast(String textToast){
+        Toast.makeText(MainActivity.this,textToast,Toast.LENGTH_SHORT).show();
+    }
+}
